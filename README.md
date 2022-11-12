93ac2bd6b02a4aba83ea971214e3b59e11453c53
“Alvin” <sdasd>
2022-11-12 12:38:43 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+Install dependencies
+
+'npm install'
+
+Run to network and locally
+
+'npm run dev --host'
+
+Run locally only
+
+'npm run dev'
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+        "primary": "#1EB854",
+                
+        "secondary": "#1FD65F",
+                
+        "accent": "#D99330",
+                
+        "neutral": "#110E0E",
+                
+        "base-100": "#171212",
+                
+        "info": "#3ABFF8",
+                
+        "success": "#36D399",
+                
+        "warning": "#FBBD23",
+                
+        "error": "#F87272",
+        },
+      },
+      
+      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter",]
+    },
+  content: ['./src/**/*.{svelte,js,ts}'],
+  plugins: [require("@tailwindcss/typography"), require('daisyui')],
+};
